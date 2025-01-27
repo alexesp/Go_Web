96fd94a7b5164f25351c42c6e0ed5bfd22253f20
alexesp <alexizaes@hotmail.com>
2025-01-27 02:36:54 +0100

    simple server

--- main.go
+++ main.go
@@ -1,11 +1,29 @@
 package main
 
 import (
+	"fmt"
+	"html/template"
 	"net/http"
 )
 
-func Home(w http.ResponseWriter, r *http.Request) {
+const portNumber = ":8088"
 
+func Home(w http.ResponseWriter, r *http.Request) {
+	//fmt.Fprintf(w, "Es la pagina de inicio")
+	renderTemplate(w, "home.page.tmpl")
+}
+func About(w http.ResponseWriter, r *http.Request) {
+	//fmt.Fprintf(w, "Es la pagina sobre!")
+	renderTemplate(w, "about.page.tmpl")
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	err := parsedTemplate.Execute(w, nil)
+	if err != nil {
+		fmt.Println("error parsin tempate:", err)
+		return
+	}
 }
 func main() {
 	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
@@ -14,6 +32,8 @@ func main() {
 	// 		fmt.Println(err)
 	// 	}
 	// })
+	//fmt.Println("Test")
 	http.HandleFunc("/", Home)
-	_ = http.ListenAndServe(":8088", nil)
+	http.HandleFunc("/about", About)
+	_ = http.ListenAndServe(portNumber, nil)
 }
